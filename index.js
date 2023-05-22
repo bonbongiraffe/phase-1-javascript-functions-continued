ce2b6dab8529e1e7b8850700ad65940846d9c31e
bonbongiraffe <francescowai@yahoo.com>
2023-05-22 12:07:11 -0400

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper = '*'){
+    return function (adj = 'special'){
+        return `You are ${wrapper}${adj}${wrapper}!`;
+    };
+}
+
+// let decWrapper = wrapAdjective('*');
+// let decAdj = decWrapper('bad');
+// console.log(decAdj);
